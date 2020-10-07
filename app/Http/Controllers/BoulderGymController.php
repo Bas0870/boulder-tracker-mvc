8fc076bb09e87cd6d258e54b4170499aab675236
Bas0870 <sentinal01@gmail.com>
2020-10-07 22:58:17 +0200

    Create very basic page with boulder gym names

---
+++ app/Http/Controllers/BoulderGymController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BoulderGym;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class BoulderGymController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Inertia\Response
+     */
+    public function index()
+    {
+        return Inertia::render('BoulderGyms/Index', [
+            'boulderGyms' => BoulderGym::all()->toArray()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\BoulderGym  $boulderGym
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BoulderGym $boulderGym)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\BoulderGym  $boulderGym
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BoulderGym $boulderGym)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\BoulderGym  $boulderGym
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BoulderGym $boulderGym)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\BoulderGym  $boulderGym
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BoulderGym $boulderGym)
+    {
+        //
+    }
+}
---
+++ resources/js/Pages/BoulderGyms/Index.vue
@@ -0,0 +1,37 @@
+<template>
+    <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Boulder Gyms
+            </h2>
+        </template>
+
+        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            <div class="mt-5 md:mt-0 md:col-span-2" v-for="boulderGym in boulderGyms">
+                <div class="px-4 py-5 sm:p-6 bg-white shadow sm:rounded-lg">
+                    <h3 class="text-lg font-medium text-gray-900">
+                        {{ boulderGym.name }}
+                    </h3>
+                </div>
+
+                <jet-section-border />
+
+            </div>
+        </div>
+
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from './../../Layouts/AppLayout'
+    import JetSectionBorder from "../../Jetstream/SectionBorder";
+
+    export default {
+        props: ['boulderGyms'],
+
+        components: {
+            AppLayout,
+            JetSectionBorder,
+        },
+    }
+</script>
